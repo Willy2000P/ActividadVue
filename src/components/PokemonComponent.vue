9cc573f5cd20f485a2dcd15df51055f340900ccd
Alejandro <alejandro.rolon.bedoya@gmail.com>
2025-03-29 18:35:41 -0500

    first commit

---
+++ src/components/PokemonComponent.vue
@@ -0,0 +1,197 @@
+<template>
+
+        <div class="block-container">
+                <div v v-if="disponible == 1" class="card card-true">
+                    
+                        <span v-if="disponible == 1" class="disponibilidad dispon-si">Disponible</span>
+                        <span v-else class="disponibilidad dispon-no">No Disponible</span>
+                        <img :src="imagen" alt="">
+                        <h3>{{ nombre }}</h3>
+                        <span :style="{
+                            'background-color': color
+                        }" class="span">{{ tipo }}</span>
+                        <ButtonPrime @click="openModal">Abrir</ButtonPrime>
+                </div>
+
+                <div v v-else class="card card-false">
+                    
+                    <span v-if="disponible == 1" class="disponibilidad dispon-si">Disponible</span>
+                    <span v-else class="disponibilidad dispon-no">No Disponible</span>
+                    <img :src="imagen" alt="">
+                    <h3>{{ nombre }}</h3>
+                    <span :style="{
+                        'background-color': color
+                    }" class="span">{{ tipo }}</span>
+                    <ButtonPrime @click="openModal">Abrir</ButtonPrime>
+
+                </div>
+        </div>
+
+
+        
+        <DialogPrime
+        :visible="abierto" 
+        maximizable 
+        modal 
+        header="Header" 
+        :style="{ width: '50rem' }" 
+        :breakpoints="{ '1199px': '75vw', '575px': '90vw' }">
+        >
+            <div >
+           
+                <div class="card card-true">
+                    
+                        <span v-if="disponible == 1" class="disponibilidad dispon-si">Disponible</span>
+                        <span v-else class="disponibilidad dispon-no">No Disponible</span>
+                        <div class="card-div">
+                            <ImagePrime class="card-image"  :src="imagen" alt="" preview />
+                        </div>
+                        <h3>{{ nombre }}</h3>
+                        <span :style="{
+                            'background-color': color
+                        }" class="span">{{ tipo }}</span>
+                        <ButtonPrime @click="closeModal">Cerrar</ButtonPrime>
+                </div>
+
+                
+        
+            </div>
+        </DialogPrime>
+  
+</template>
+
+<script>
+export default {
+    name: "PokemonComponent",
+
+    data(){
+    return {
+
+      abierto: false
+    }
+    },
+
+    props: {
+        id: Number,
+        imagen: String,
+        nombre: String,
+        tipo: String,
+        color: String,
+        disponible: Number
+    },
+
+    methods: {
+        alertPokemon: function (nombre) {
+            alert(`Ha seleccionado el Pokemon ${nombre}`)
+            return
+        },
+
+        closeModal: function() {
+            this.abierto = false
+        },
+
+        openModal: function() {
+            this.abierto = true
+        }
+
+
+        
+    }
+
+}
+</script>
+
+<style scope>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+
+
+.card-div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+
+.card-image {
+    width: 100px;
+}
+
+.block-container {
+    display: flex;
+    padding: 1rem;
+
+}
+.card {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    padding: 10px;
+    position: relative;
+    
+}
+
+img {
+    width: 100%;
+}
+
+.span {
+
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    color: black;
+    width: 100%;
+
+}
+
+.disponibilidad {
+    position: absolute;
+    top: 0;
+    right: 0;
+    background-color: green;
+    color: white;
+    padding: 5px;
+    border-radius: 12px;
+    margin: 4px;
+}
+
+.dispon-si {
+    background-color: green;
+    
+
+}
+
+
+.dispon-no {
+    background-color: red;
+    
+
+}
+
+.card-true {
+    transition: 0.3s ease-in-out; 
+    cursor: pointer;
+}
+
+.card-true:hover {
+    box-shadow: 10px 5px 5px rgb(173, 173, 173);
+    transform: scale(1.1);
+    box-shadow: 0 0 15px rgba(0, 255, 0, 0.5);
+}
+
+
+.card-false {
+    transition: 0.3s ease-in-out;
+    background-color: lightgray;
+    cursor: not-allowed;
+}
+.card-false:hover {
+    background-color: orange; 
+    opacity: 0.5; 
+    transform: scale(0.9); 
+   
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,37 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
+
+//Importacion de paquetes bases
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 
 
-createApp(App).use(router).mount('#app')
+//Importacion de Componentes
+import Button from "primevue/button"
+import Dialog from 'primevue/dialog';
+import Image from 'primevue/image';
+
+
+
+
+const app = createApp(App).use(router)
+app.use(VueAxios, axios);
+
+//Uso de componentes
+
+app.component('ButtonPrime', Button);
+app.component('DialogPrime', Dialog);
+app.component('ImagePrime', Image);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+
+app.mount('#app')
+export {app}
